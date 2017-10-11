248bab6a60a52b9374287a77416ae9ccbba4732c
RedDevil91 <b.attila91@gmail.com>
2017-10-11 20:14:24 +0200

    table preprocess update

--- preprocess_images.py
+++ preprocess_images.py
@@ -1,11 +1,7 @@
 import cv2
 import numpy as np
 
-roi_size = 640
-corners = np.float32([[0,          0],
-                      [roi_size,   0],
-                      [roi_size,   roi_size],
-                      [0,          roi_size]])
+from win_interface import screenshot
 
 
 class NoTableError(BaseException):
@@ -13,7 +9,6 @@ class NoTableError(BaseException):
 
 
 class RegionOfInterest(object):
-    padding = 0
 
     def __init__(self, contour, moment, approx):
         self.contour = contour
@@ -21,29 +16,24 @@ class RegionOfInterest(object):
         self.approx = np.reshape(approx, (4, 2)).astype(np.float32)
 
         # sorting corners for perspective transform
-        self.corners = np.zeros(self.approx.shape, dtype=np.float32)
+        self.corners = np.zeros(self.approx.shape, dtype=np.uint32)
         corner_diff = np.diff(self.approx, axis=1)
         corner_sum = np.sum(self.approx, axis=1)
         self.corners[0] = self.approx[np.argmin(corner_sum)]
         self.corners[1] = self.approx[np.argmin(corner_diff)]
         self.corners[2] = self.approx[np.argmax(corner_sum)]
         self.corners[3] = self.approx[np.argmax(corner_diff)]
-
-        # add padding to table
-        self.corners[0][0] -= self.padding
-        self.corners[0][1] -= self.padding
-        self.corners[1][0] += self.padding
-        self.corners[1][1] -= self.padding
-        self.corners[2][0] += self.padding
-        self.corners[2][1] += self.padding
-        self.corners[3][0] -= self.padding
-        self.corners[3][1] += self.padding
         return
 
+    def checkIfSquare(self):
+        width = self.corners[2][0] - self.corners[0][0]
+        height = self.corners[2][1] - self.corners[0][1]
+        return width == height
 
-def preprocess_image(input_image):
+
+def preprocess_image(input_image, threshold, algo=cv2.THRESH_BINARY_INV):
     gray = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(gray, threshold, 255, algo)
     return thresh
 
 
@@ -58,16 +48,15 @@ def get_roi(input_image):
         # calculate the perimeter and approximate the contour with a polygon
         perim = cv2.arcLength(cont, True)
         approx = cv2.approxPolyDP(cont, 0.04 * perim, True)
-        # calculate the area
-        contour_area = cv2.contourArea(cont)
-        hull_area = cv2.contourArea(cv2.convexHull(cont))
-        area_ratio = contour_area / float(hull_area)
+        # we are searching for a square
+        if len(approx) != 4:
+            continue
         # if the approximation has 4 vertex and the area ratio is greater than 0.9 then
         # we assume that it's a square
-        if len(approx) == 4 and area_ratio > 0.9:
-            M = cv2.moments(cont)
-            roi_img = RegionOfInterest(cont, M, approx)
-            # the greatest square will be the sudoku table itself
+        M = cv2.moments(cont)
+        roi_img = RegionOfInterest(cont, M, approx)
+        if roi_img.checkIfSquare():
+            # the greatest square will be the chess table itself
             break
     else:
         # if there is no square
@@ -76,17 +65,16 @@ def get_roi(input_image):
 
 
 if __name__ == '__main__':
-    raw_image = cv2.imread('pictures/board_start2.png')
-    thresholded = preprocess_image(raw_image)
+    raw_image = cv2.imread('pictures/screen_00.png')
+    thresholded = preprocess_image(raw_image, 220)
     roi = get_roi(thresholded)
+    
+    # increment is needed because the table size is 639x639
+    table_image = raw_image[roi.corners[0][1]:roi.corners[2][1]+1, roi.corners[0][0]:roi.corners[2][0]+1]
 
-    # use perspective transformation on the image to get the table only
-    pers_matrix = cv2.getPerspectiveTransform(roi.corners, corners)
-    table_image = cv2.warpPerspective(raw_image, pers_matrix, (roi_size, roi_size))
+    gray = preprocess_image(table_image, 30, algo=cv2.THRESH_BINARY)
 
-    cv2.imshow('table', table_image)
-
-    gray = cv2.cvtColor(table_image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("table", gray)
 
     for row in range(8):
         for col in range(8):
@@ -94,7 +82,9 @@ if __name__ == '__main__':
             row_end = (row+1)*80
             col_start = col*80
             col_end = (col+1)*80
-            cv2.imwrite('squares/square_%d_%d.png' % (row, col), gray[row_start:row_end, col_start:col_end])
+            square = gray[row_start:row_end, col_start:col_end]
+            shape = square.shape
+            cv2.imwrite('squares/square_%d_%d.png' % (row, col), square)
 
     while True:
         key = cv2.waitKey(1) & 0xFF
